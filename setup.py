29045b4ff85857679a920efab492ab0873ea2d28
Jason Hobbs <jason.hobbs@gmail.com>
2016-05-28 21:26:44 -0500

    Add pyaudio as install requirement.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='remorse',
-      version='0.2',
+      version='0.3',
       description='Morse code player',
       url='http://github.com/jhobbs/remorse',
       author='Jason Hobbs',
       author_email='jason.hobbs@gmail.com',
       license='Apache License 2.0',
       packages=['remorse'],
-      install_requires=['audiogen'],
+      install_requires=['audiogen', 'pyaudio'],
       scripts=['scripts/remorse'])
