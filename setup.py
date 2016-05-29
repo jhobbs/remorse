a9ed1752def29161723c65ae6e3f9a49d3f7f96c
Jason Hobbs <jason.hobbs@gmail.com>
2016-05-28 21:18:17 -0500

    Fix install_requires.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='remorse',
-      version='0.1',
+      version='0.2',
       description='Morse code player',
       url='http://github.com/jhobbs/remorse',
       author='Jason Hobbs',
       author_email='jason.hobbs@gmail.com',
       license='Apache License 2.0',
       packages=['remorse'],
-      install_requeres=['audiogen'],
+      install_requires=['audiogen'],
       scripts=['scripts/remorse'])
