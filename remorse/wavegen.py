f55e18c1ad7185269dfd62a1986c528a93bf0ddd
Jason Hobbs <jason.hobbs@gmail.com>
2016-06-11 22:43:43 -0500

    Add some documentation on how it works.

--- remorse/wavegen.py
+++ remorse/wavegen.py
@@ -8,22 +8,36 @@ FADE_FRAMES = int(BITRATE * FADE_LENGTH)
 
 
 def sine(frequency, length):
-    """Generate a sine wave for use with pyaudio.
+    """Generate a sine wave in 8-bit unsigned PCM format.
 
     Uses linear fading at the beginning and end to avoid click noise.
+
+    Good reference on how simple digital sound generation works:
+    http://www.cs.nmsu.edu/~rth/cs/computermusic/Simple%20sound%20generation.html
+
+    We use s_n = (v * sin(2*pi*f*n/sr)) + 128 where:
+    - n is the sample number.
+    - s_n is sample n.
+    - f is frequency in hertz.
+    - sr is the sample rate in samples per second.
+    - v is the volume in the range of 0 to 127.
+
+    Adding 128 serves to center the samples at 128, which is silence in 8-bit
+    unsigned PCM format.
     """
     wave_data = ''
     number_of_frames = int(BITRATE * length)
-    factor = float(frequency) * (math.pi * 2) / BITRATE
-    for x in xrange(number_of_frames):
-        if x < FADE_FRAMES:
-            volume_factor = float(x) / FADE_FRAMES
-        elif number_of_frames - x < FADE_FRAMES:
-            volume_factor = float(number_of_frames - x) / FADE_FRAMES
+    factor = (float(frequency) * (math.pi * 2)) / BITRATE
+    for n in xrange(number_of_frames):
+        if n < FADE_FRAMES:
+            volume_factor = float(n) / FADE_FRAMES
+        elif number_of_frames - n < FADE_FRAMES:
+            volume_factor = float(number_of_frames - n) / FADE_FRAMES
         else:
             volume_factor = 1
         volume = 127 * volume_factor
-        wave_data += chr(int(math.sin(x * factor) * volume + 128))
+        zero_centered = int(math.sin(n * factor) * volume)
+        wave_data += chr(zero_centered + 128)
     return wave_data
 
 
